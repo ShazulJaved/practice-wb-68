56963712a8a473cbe37f7cfa8c2559e8c4d1a976
ShazulJaved <shazul03266646@gmail.com>
2024-06-20 01:57:16 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+stu_tm = prompt("enter tm");
+stu_obt = prompt("enter obt");
+result = stu_obt/stu_tm*100
+alert(`You have got it ${result} %`)
+document.write(`<h1> You have got it ${result} %<h1>`)
